63512453d8b4762060ab26d8aa2f03831a979771
Cas van Cooten <chvancooten@gmail.com>
2023-06-04 13:17:17 +0200

    Bump versions, fix failing Nuclei install

--- setup.sh
+++ setup.sh
@@ -60,16 +60,16 @@ cd "$toolsDir" || { echo "Something went wrong"; exit 1; }
 # Various apt packages
 echo "[*] Running apt update and installing apt-based packages, this may take a while..."
 apt-get update >/dev/null
-apt-get install -y xvfb dnsutils nmap python3.5 python2 python3-pip curl wget unzip git libfreetype6 libfontconfig1 >/dev/null
+apt-get install -y xvfb dnsutils nmap python3.9 python2 python3-pip curl wget unzip git libfreetype6 libfontconfig1 >/dev/null
 rm -rf /var/lib/apt/lists/*
 
 # Golang
 go version &> /dev/null
 if [ $? -ne 0 ]; then
     echo "[*] Installing Golang..."
-    wget -q https://golang.org/dl/go1.19.4.linux-amd64.tar.gz
-    tar -xvf go1.19.4.linux-amd64.tar.gz -C /usr/local >/dev/null
-    rm -rf ./go1.19.4.linux-amd64.tar.gz >/dev/null
+    wget -q https://golang.org/dl/go1.20.4.linux-amd64.tar.gz
+    tar -xvf go1.20.4.linux-amd64.tar.gz -C /usr/local >/dev/null
+    rm -rf ./go1.20.4.linux-amd64.tar.gz >/dev/null
     export GOROOT="/usr/local/go"
     export GOPATH="$homeDir/go"
     export PATH="$PATH:${GOPATH}/bin:${GOROOT}/bin:${PATH}"
@@ -115,16 +115,16 @@ wget -q https://raw.githubusercontent.com/Bo0oM/fuzz.txt/master/fuzz.txt -O $too
 
 # HTTPX
 echo "[*] Installing HTTPX..."
-wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.2.5/httpx_1.2.5_linux_amd64.zip
-unzip -j httpx_1.2.5_linux_amd64.zip -d /usr/bin/ httpx >/dev/null
-rm httpx_1.2.5_linux_amd64.zip
+wget -q https://github.com/projectdiscovery/httpx/releases/download/v1.3.1/httpx_1.3.1_linux_amd64.zip
+unzip -j httpx_1.3.1_linux_amd64.zip -d /usr/bin/ httpx >/dev/null
+rm httpx_1.3.1_linux_amd64.zip
 
 # Amass
 echo "[*] Installing Amass..."
-wget -q https://github.com/OWASP/Amass/releases/download/v3.21.2/amass_linux_amd64.zip
-unzip -q amass_linux_amd64.zip
-mv amass_linux_amd64 amass
-rm amass_linux_amd64.zip
+wget -q https://github.com/owasp-amass/amass/releases/download/v3.23.2/amass_Linux_amd64.zip
+unzip -q amass_Linux_amd64.zip
+mv amass_Linux_amd64 amass
+rm amass_Linux_amd64.zip
 cp $toolsDir/amass/amass /usr/bin/amass
 
 # Gf-patterns
