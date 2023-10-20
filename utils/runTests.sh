d8d452931361c024ec11147a6d6328e9d24d8ece
theinit01 <tushar3145@gmail.com>
2023-10-19 20:57:30 -0400

    updated runTests.sh

--- utils/runTests.sh
+++ utils/runTests.sh
@@ -70,10 +70,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-# GoBuster
-gobuster -h &> /dev/null
+## ffuf
+ffuf -h &> /dev/null
 if [ $? -ne 0 ]; then
-    echo "Error - GoBuster not (properly) installed"
+    echo "Error - ffuf not (properly) installed"
     exit 1
 fi
 
